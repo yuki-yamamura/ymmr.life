7accef39d2c042960073ca88a66f2221ef0c2e2a
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-06-19 06:02:41 +0900

    chore: rename setup file for Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    setupFiles: ["./vitest-setup.js"],
+    globals: true,
+    setupFiles: ["./vitest.setup.ts"],
   },
 });
