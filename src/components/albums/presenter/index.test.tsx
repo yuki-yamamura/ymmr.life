b7accefbf6179c2e468b99b93390bc9769b29f77
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-06-19 05:09:30 +0900

    learn how to use stories within Vitest's test

---
+++ src/components/albums/presenter/index.test.tsx
@@ -0,0 +1,23 @@
+import { describe, test, expect } from "vitest";
+import { render, screen, within } from "@testing-library/react";
+import { composeStory } from "@storybook/react";
+import Meta, { Default as AlbumsStory } from "./index.stories";
+
+const Albums = composeStory(AlbumsStory, Meta);
+
+describe("Albums", () => {
+  test("should render a list of albums", () => {
+    // arrange
+    render(<Albums />);
+
+    // act
+    const list = screen.getByRole<HTMLUListElement>("list");
+    const items = within(list).getAllByRole("listitem");
+
+    // assert
+    expect(items).toHaveLength(3);
+    expect(items[0].textContent).toBe("quidem molestiae enim");
+    expect(items[1].textContent).toBe("sunt qui excepturi placeat culpa");
+    expect(items[2].textContent).toBe("omnis laborum odio");
+  });
+});
