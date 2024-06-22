c7a633320780e4e5e9312c513c8cb3179db07f1a
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-06-23 07:11:37 +0900

    fix: fix invalid import path

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom/vitest";
 
 import { beforeAll, afterEach, afterAll } from "vitest";
-import { server } from "./src//mocks/node";
+import { server } from "@/mocks/node";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
