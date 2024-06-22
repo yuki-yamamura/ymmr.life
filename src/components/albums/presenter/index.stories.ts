050cfc3628cbff7c1b723659a3781ea72293656c
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-06-23 08:41:49 +0900

    test: set up Storybook's test runner and move component tests into it

--- src/components/albums/presenter/index.stories.ts
+++ src/components/albums/presenter/index.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Component from ".";
 import { fakeAlbums } from "../__mock__/fixture";
+import { expect, within } from "@storybook/test";
 
 type Story = StoryObj<typeof Component>;
 
@@ -12,5 +13,14 @@ export default {
 export const Default: Story = {
   args: {
     albums: fakeAlbums,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    expect(canvas.getByText("quidem molestiae enim")).toBeInTheDocument();
+    expect(
+      canvas.getByText("sunt qui excepturi placeat culpa")
+    ).toBeInTheDocument();
+    expect(canvas.getByText("omnis laborum odio")).toBeInTheDocument();
   },
 };
