908ba9af53f183e73bcaad336f46d74c8dbcb327
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-06-24 06:40:16 +0900

    feat: add convert function using unified

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,35 @@
+import { MicroCMSClient } from "@/lib/microcms";
+import { convertMarkdownIntoJSX } from "@/lib/unified";
+import { Post } from "@/types/Post";
+import { MicroCMSContentId } from "microcms-js-sdk";
+
+import "prism-themes/themes/prism-night-owl.min.css";
+
+type Params = MicroCMSContentId;
+
+type Props = {
+  params: Params;
+};
+
+const client = new MicroCMSClient<Post>("posts");
+
+export async function generateStaticParams(): Promise<Params[]> {
+  const posts = await client.fetchList();
+
+  return posts.map(({ id }) => ({
+    id,
+  }));
+}
+
+const Page = async ({ params: { id } }: Props) => {
+  const post = await client.fetchListDetailById({ id });
+
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <main>{convertMarkdownIntoJSX(post.body)}</main>
+    </>
+  );
+};
+
+export default Page;
---
+++ src/lib/unified/index.ts
@@ -0,0 +1,28 @@
+import React from "react";
+import rehypePrism from "rehype-prism";
+import rehypeReact, { Options } from "rehype-react";
+import remarkBreaks from "remark-breaks";
+import remarkGfm from "remark-gfm";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
+import * as prod from "react/jsx-runtime";
+
+export async function convertMarkdownIntoJSX(
+  markdownContent: string
+): Promise<JSX.Element> {
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkBreaks)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypePrism)
+    .use(rehypeReact, {
+      Fragment: React.Fragment,
+      jsx: prod.jsx,
+      jsxs: prod.jsxs,
+    } as Options)
+    .process(markdownContent);
+
+  return file.result;
+}
