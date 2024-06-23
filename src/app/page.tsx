35fc826cc7872f3d3181c24fcd838612aa650204
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-06-23 10:08:19 +0900

    chore: remove Album component
    Because this component was aimed at learning Storybook, now it is
    unused.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-import { Albums } from "@/components/albums";
 import { PostList } from "@/components/post-list";
 
 export default function Home() {
-  return (
-    <>
-      <PostList />
-      <hr className="my-4" />
-      <Albums />
-    </>
-  );
+  return <PostList />;
 }
--- src/components/albums/container/index.tsx
+++
@@ -1,8 +0,0 @@
-import Component from "../presenter";
-import { fetchAlbums } from "./service";
-
-export default async function Container() {
-  const albums = await fetchAlbums();
-
-  return <Component albums={albums} />;
-}
