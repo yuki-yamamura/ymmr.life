a402597260ce47ca713fe221eacf90c97ade07da
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-09-23 10:12:25 +0900

    hello, world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1>Hello, World!</h1>
 
         <div className={styles.ctas}>
           <a
