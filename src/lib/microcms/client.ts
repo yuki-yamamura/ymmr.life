03a909a28111eaf7b62a5f0bb9740687df190f0b
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-10-12 13:11:24 +0900

    feat: add cms client to featch data

---
+++ src/lib/microcms/client.ts
@@ -0,0 +1,16 @@
+import { createClient } from "microcms-js-sdk";
+
+const serviceDomain = process.env.MICROCMS_SERVICE_DOMAIN;
+const apiKey = process.env.MICROCMS_API_KEY;
+
+if (!serviceDomain) {
+  throw new Error("Missing env.MICROCMS_SERVICE_DOMAIN");
+}
+if (!apiKey) {
+  throw new Error("Missing env.MICROCMS_API_KEY");
+}
+
+export const client = createClient({
+  serviceDomain,
+  apiKey,
+});
